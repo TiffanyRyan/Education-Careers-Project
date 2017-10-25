2f75ff7f525a18da233b2e15fd2b85e331d300ee
emmassr <31829603+emmassr@users.noreply.github.com>
2017-10-25 18:53:44 +0100

    revised

---
+++ Job Matching.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("Info").click(function(){
+
+$(this).hide();
+});
+});
+$(document).ready(function(){
+    $(".btn-info").click(function(){
+        $(this).button('toggle');
+    });   
+});
